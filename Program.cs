340a61ad2335ab7e12509ca4f4e4e2747480f4b5
szkrecsi <ecsenszki.judit@gmail.com>
2017-06-11 23:12:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+            Array.Sort(words);
+            string s = string.Join(" ", words);
+            Console.WriteLine(s);
+        }
+    }
+}
